8249a79efe6882c0ba50c8db7a3072e655548e1b
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-10-27 00:10:29 +0900

    크레인 인형 뽑기 게임

--- src/main/java/Lv1/_58_크레인_인형_뽑기_게임/Solution.java
+++ src/main/java/Lv1/_58_크레인_인형_뽑기_게임/Solution.java
@@ -29,38 +29,42 @@ package Lv1._58_크레인_인형_뽑기_게임;
 //        인형의 처음 상태는 문제에 주어진 예시와 같습니다. 크레인이 [1, 5, 3, 5, 1, 2, 1, 4] 번 위치에서 차례대로 인형을 집어서 바구니에 옮겨 담은 후, 상태는 아래 그림과 같으며 바구니에 담는 과정에서 터트려져 사라진 인형은 4개 입니다.
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        List<Integer> result = new ArrayList<>();
-
+        //참고
+        // 1. 연속된 인형들이 제거된 횟수.
         int answer = 0;
+        // 2. 인형을 담을 stack 바구니.
+        Stack<Integer> stack = new Stack<>();
 
-        for(int num : moves) {
-            for (int i = 0; i < board.length; i++) {
-                if(0 != board[i][num-1]) {
-                    result.add(board[i][num-1]);
-                    board[i][num-1] = 0;
-                    if (result.size() < 2) {
+        // 3. moves의 크기는 크레인의 총 이동횟수.
+        for(int i =0;i<moves.length;i++){
+            // 3-1. j 인덱스를 이용해 보드의 행을 탐색. 열은 moves의 원소를 이용해 탐색.
+            for(int j=0;j<board.length;j++){
+                // 3-2. j행의 moves의 크레인위치 열에 해당하는 값에 인형이 존재한다면.
+                if(board[j][moves[i]-1] != 0 ) {
+                    // 3-3. stack이 비어있지 않고, 현재 스택의 최상단에 있는 인형과 크레인으로 뽑은 인형이 같다면
+                    if(!stack.empty() && stack.peek() == board[j][moves[i]-1]){
+                        // 인형들을 제거하는 횟수 증가.
+                        answer++;
+                        // 바구니에 있는 인형을 제거.
+                        stack.pop();
+                        // 크레인으로 뽑은 인형을 0으로 만들어 없애준다.
+                        board[j][moves[i]-1] = 0;
                         break;
-                    }else {
-                        for(int j = 1 ; j < result.size()-1 ; j++){
-                            if (Objects.equals(result.get(j), result.get(j + 1))) {
-                                result.remove(result.get(j));
-                                result.remove(result.get(j));
-                                answer += 2;
-                            }
-                        }
+                        // 3-4. 그 외의 경우는 인형을 바구니에 담은 후 0으로 없애준다.
+                    }else{
+                        stack.push(board[j][moves[i]-1]);
+                        board[j][moves[i]-1] = 0;
+                        break;
                     }
-
                 }
             }
         }
-
-        return answer;
+        // 4. 없앤 인형의 수 = 인형을 없앤 수 * 2.
+        return answer*2;
     }
 
     public static void main(String[] args) {
