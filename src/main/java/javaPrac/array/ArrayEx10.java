3fb0294f62252190627479994165626a22d7ad43
leesu <leesulgi66@naver.com>
2023-01-07 19:33:34 +0900

    ArrayEx10

---
+++ src/main/java/javaPrac/array/ArrayEx10.java
@@ -0,0 +1,32 @@
+package javaPrac.array;
+
+public class ArrayEx10 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for(int i=0; i<numArr.length; i++){
+            System.out.print(numArr[i] = (int) (Math.random() * 10));
+        }
+        System.out.println();
+
+        for(int i=0; i<numArr.length; i++) {
+            boolean changed = false;  // 자리바꿈이 발생했는지를 체크한다.
+
+            for(int j=0; j<numArr.length-1-i; j++) {
+                if(numArr[j] > numArr[j+1]) {
+                    int tmp = numArr[j];
+                    numArr[j] = numArr[j+1];
+                    numArr[j+1] = tmp;
+                    changed = true;  // 자리바꿈이 발생했으니 true로 변경
+                }
+            }
+
+            if(!changed) break;  // 자리바꿈이 없으면 반복문을 벗어난다.
+
+            for(int k=0; k<numArr.length; k++)
+                System.out.print(numArr[k]);
+            System.out.println();
+
+        }
+    }
+}
