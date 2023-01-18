989d45686bd40c7c2ff8eea02bc5fa8c85afd87a
leesu <leesulgi66@naver.com>
2023-01-18 18:11:34 +0900

    TvTest

--- src/main/java/javaPrac/object_oriented_programming/tv_test/TvTest.java
+++ src/main/java/javaPrac/object_oriented_programming/tv_test/TvTest.java
@@ -1,16 +1,6 @@
 package javaPrac.object_oriented_programming.tv_test;
 
-class Tv {
-    // Tv의 속성 (맴버변수)
-    String color;  // 색상
-    boolean power;  // 전원상태 (on/off)
-    int channel;  // 채널
-
-    //Tv의 기능(메서드)
-    void power() { power = !power;}  // Tv를 켜거나 끄는 기능을 하는 메서드
-    void channelUp() { ++channel; }  // Tv의 채널을 높이는 기능을 하는 메서드
-    void channerDown() { --channel; }  // Tv의 채널을 낮추는 기능을 하는 메서드
-}
+import javaPrac.object_oriented_programming.Tv;
 
 class TvTest {
     public static void main(String[] args) {
