922c434d56547f18b4f9fffd5dad9df7d03143fa
leesu <leesulgi66@naver.com>
2023-02-03 18:54:00 +0900

    StaticImportEx1

---
+++ src/main/java/javaPrac/ch07_object_oriented_programming2/_03_package_and_import/StaticImportEx1.java
@@ -0,0 +1,13 @@
+package javaPrac.ch07_object_oriented_programming2._03_package_and_import;
+
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class StaticImportEx1 {
+    public static void main(String[] args) {
+        //System.out.println(Math.random());
+        out.println(random());
+        //System.out.println("Math.PI : "+Math.PI);
+        out.println("Math.PI : "+Math.PI);
+    }
+}
