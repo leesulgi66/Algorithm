b806d0685f9f4cbfeceb187ea67dce6f81b46afc
leesu <leesulgi66@naver.com>
2023-02-10 16:19:52 +0900

    InnerEx3

---
+++ src/main/java/javaPrac/ch07_object_oriented_programming2/_08_inner_class/InnerEx3.java
@@ -0,0 +1,31 @@
+package javaPrac.ch07_object_oriented_programming2._08_inner_class;
+
+public class InnerEx3 {
+    private int outerIv = 0;
+    static int outerCv = 0;
+
+    class InstatnceInner {
+        int iiv = outerIv;  // 외부 클래스의 private멤버도 접근 가능하다.
+        int iiv2 = outerCv;
+    }
+
+    static class StaticInner {
+        // 스태틱 클래스는 외부 클래스의 인스턴스멤버에 접근할 수 없다.
+        // int siv = outerIv;
+        static int scv = outerCv;
+    }
+
+    void myMethod() {
+        int lv = 0;
+        final int Lv = 0;   // JDK1.8부터 final 생략 가능
+
+        class LocalInner {
+            int liv = outerIv;
+            int liv2 = outerCv;
+
+            // 외부 클래스의 지역변수는 final이 붙은 변수(상수)만 접근 가능하다.
+            int liv3 = lv;  //<--- 원래는 에러이지만 JDK1.8부터는 허용됨.
+            int liv4 = Lv;
+        }
+    }
+}
