32bf706e53eb418ebdfd3f3835686c99d61ec7e0
leesu <leesulgi66@naver.com>
2023-02-11 02:48:49 +0900

    InnerEx7

---
+++ src/main/java/javaPrac/ch07_object_oriented_programming2/_08_inner_class/anonymous_class/InnerEx7.java
@@ -0,0 +1,17 @@
+package javaPrac.ch07_object_oriented_programming2._08_inner_class.anonymous_class;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class InnerEx7 {
+    public static void main(String[] args) {
+        Button b = new Button("Start");
+        b.addActionListener(new EventHandler());
+    }
+}
+
+class EventHandler implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("ActionEvent occurred!!!");
+    }
+}
