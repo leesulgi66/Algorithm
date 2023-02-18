66a3b738e7014c03c81b588752a7892bf9fa570b
leesu <leesulgi66@naver.com>
2023-02-18 14:29:58 +0900

    StringEx2

---
+++ src/main/java/javaPrac/ch09_java_lang_package_and_utill_classes/StringEx2.java
@@ -0,0 +1,12 @@
+package javaPrac.ch09_java_lang_package_and_utill_classes;
+
+public class StringEx2 {
+    public static void main(String[] args) {
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+
+        // "AAA"를 담고있는 Stirng인스턴스가 하나 생성된 후, 참조변수 s1,s2,s3는 모두 이 String인스턴스를 참조하게 된다.
+    }
+}
