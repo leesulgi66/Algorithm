d769dd7e12b4bfe2b23b03d9860fb67bd1017a38
leesu <leesulgi66@naver.com>
2023-03-22 20:50:18 +0900

    AsciiPrint

---
+++ src/main/java/javaPrac/ch11_collections_framework/_08_TreeSet/AsciiPrint.java
@@ -0,0 +1,10 @@
+package javaPrac.ch11_collections_framework._08_TreeSet;
+
+public class AsciiPrint {
+    public static void main(String[] args) {
+        char ch = ' ';
+        // 공백 ('  ')이후의 문자들을 출력한다.
+        for(int i=0; i<95; i++)
+            System.out.print(ch++);
+    }
+}
