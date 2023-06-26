1ea52a15fbfde7aa5dbd0f56f630d765dda7d4bc
leesu <leesulgi66@naver.com>
2023-06-26 21:43:41 +0900

    스택 수열.java

--- src/main/java/Algorithm/_07_알고리즘_코딩테스트_문제풀이_강의_11_스택으로_수열_만들기/P1874_스택수열.java
+++ src/main/java/Algorithm/_07_알고리즘_코딩테스트_문제풀이_강의_11_스택으로_수열_만들기/P1874_스택수열.java
@@ -1,2 +1,41 @@
-package Algorithm._07_알고리즘_코딩테스트_문제풀이_강의_11_스택으로_수열_만들기;public class P1874_스택수열 {
+package Algorithm._07_알고리즘_코딩테스트_문제풀이_강의_11_스택으로_수열_만들기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P1874_스택수열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        for(int i=0; i<N; i++) {
+            A[i] = sc.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+        boolean result = true;
+        StringBuffer bf = new StringBuffer();
+        for(int i=0; i<A.length; i++) {
+            int su = A[i];
+            if(su >= num) {
+                while(su >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }
+            else {
+                int n = stack.pop();
+                if(n > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        if(result) System.out.println(bf);
+    }
 }
