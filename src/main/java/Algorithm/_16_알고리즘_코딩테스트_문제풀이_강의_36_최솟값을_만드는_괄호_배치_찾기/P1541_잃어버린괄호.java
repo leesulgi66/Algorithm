4046701522343e6c2c6d9adbd2e37464b551ccbc
leesu <leesulgi66@naver.com>
2023-07-05 20:09:20 +0900

    일어버린 괄호.java

---
+++ src/main/java/Algorithm/_16_알고리즘_코딩테스트_문제풀이_강의_36_최솟값을_만드는_괄호_배치_찾기/P1541_잃어버린괄호.java
@@ -0,0 +1,28 @@
+package Algorithm._16_알고리즘_코딩테스트_문제풀이_강의_36_최솟값을_만드는_괄호_배치_찾기;
+
+import java.util.Scanner;
+
+public class P1541_잃어버린괄호 {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String example = sc.nextLine();
+        String[] str = example.split("-");
+        for(int i=0; i<str.length; i++) {
+            int temp = mySum(str[i]);
+            if(i==0) answer = answer + temp;
+            else answer = answer - temp;
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int mySum(String s) {
+        int sum =0;
+        String[] temp = s.split("[+]");
+        for(int i=0; i<temp.length; i++) {
+            sum = sum + Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
