78178cf9c67096dedc0eb82ab6b0f4dd392f7bcb
leesu <leesulgi66@naver.com>
2023-07-10 17:59:15 +0900

    refactoring

---
+++ src/main/java/Algorithm/_코딩테스트_문제풀이/_05_알고리즘_코딩테스트_문제풀이_강의_7_주몽의_명령/P1940_주몽.java
@@ -0,0 +1,34 @@
+package Algorithm._코딩테스트_문제풀이._05_알고리즘_코딩테스트_문제풀이_강의_7_주몽의_명령;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.StringTokenizer;
+
+public class P1940_주몽 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int count = 0;
+        int i=0;    // A[0] -> Min
+        int j=N-1;  // A[N-1] -> Max
+
+        while(i<j) {
+            if(A[i]+A[j]<M) i++;
+            else if(A[i]+A[j]>M)j--;
+            else{
+                count++;
+                i++; j--;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/Algorithm/_코딩테스트_문제풀이/_08_알고리즘_코딩테스트_문제풀이_강의_13_카드게임/P2164_카드.java
@@ -0,0 +1,23 @@
+package Algorithm._코딩테스트_문제풀이._08_알고리즘_코딩테스트_문제풀이_강의_13_카드게임;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class P2164_카드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> myQueue = new LinkedList<>();
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++) {
+            myQueue.add(i);
+        }
+
+        while(myQueue.size()>1) {
+            myQueue.poll();
+            int temp = myQueue.poll();
+            myQueue.add(temp);
+        }
+        System.out.println(myQueue.poll());
+    }
+}
