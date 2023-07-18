4312d1657590c4b46976eaf117f63e6edaea1c97
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-07-18 20:42:25 +0900

    BFS.java

---
+++ src/main/java/Algorithm/탐색_종류/BFS.java
@@ -0,0 +1,53 @@
+package Algorithm.탐색_종류;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    static int[][] arr;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> arrayList;
+    static ArrayList<Integer>order;
+    public static void main(String[] args) {
+        arr = new int[][] {{1,2},{1,3},{2,5},{2,6},{3,4},{4,6}};
+        visited = new boolean[7];
+        arrayList = new ArrayList<>();
+        order = new ArrayList<>();
+        for(int i=0; i<7; i++) {
+            arrayList.add(new ArrayList<>());
+        }
+
+        for(int[] i : arr) {
+            arrayList.get(i[0]).add(i[1]);
+        }
+
+        BFS(1);
+
+        System.out.println(Arrays.toString(visited)); // 방문 배열 모두 방문 했는가
+        System.out.println(order);  // 탐색 순서.
+
+    }
+
+    static void BFS(int a) {
+        Queue<Integer> queue = new LinkedList<>();
+        if(visited[a]){
+            return;
+        }
+        visited[a]=true;
+        queue.offer(a);
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            visited[now]=true;
+            order.add(now);
+            for(int i : arrayList.get(now)) {
+                if(!visited[i]) {
+                    queue.add(i);
+                }
+            }
+        }
+
+    }
+}
