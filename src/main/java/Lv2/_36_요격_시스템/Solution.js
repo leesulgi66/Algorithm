281b396d662623864f1a8dbf26a60c4b129dc62c
Ori <leesulgi66@naver.com>
2023-08-27 05:03:25 +0900

    요격 시스템.js

---
+++ src/main/java/Lv2/_36_요격_시스템/Solution.js
@@ -0,0 +1,18 @@
+function solution(targets) {
+    let answer = 1;
+    targets.sort((a,b) => b[0] - a[0]);
+    let num = targets[0][0];
+    console.log(targets);
+
+    for(let i=1; i<targets.length; i++) {
+        if(targets[i][1] <= num) {
+            console.log("checkmate");
+            num = targets[i][0];
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution(targets=[[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]));
